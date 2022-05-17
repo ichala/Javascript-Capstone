8fc51aa8298513f316ff6ac8e60428c75df679dc
Kwamboka <kwambokaj2.jk@gmail.com>
2022-05-17 11:41:03 +0300

    Add Event listener for popup window

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './reset.css';
+import * as Elements from './modules/Dom.js';
+
+Elements.popupBtn.addEventListener('click', (e) => {
+  const openModal = () => {
+    Elements.modal.style.display = 'block';
+  };
+  openModal();
+});
+
+Elements.closeBtn.addEventListener('click', (e) => {
+  const closeModal = () => {
+    Elements.modal.style.display = 'none';
+  };
+  closeModal();
+});
