a699f67e0934df8ad4d778439ea2c2677c2ded0f
Kwamboka <kwambokaj2.jk@gmail.com>
2022-05-17 14:35:33 +0300

    Add function call file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './reset.css';
 import * as Elements from './modules/Dom.js';
+import viewMeal from './modules/Functions.js';
+import getMeal from './modules/Api.js';
+
+getMeal(url).then((data) => viewMeal(data));
 
 Elements.popupBtn.addEventListener('click', () => {
   const openModal = () => {
