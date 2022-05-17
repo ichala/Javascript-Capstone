c862156e6b6844226cef0346870158e4b6fc6ca3
Kwamboka <kwambokaj2.jk@gmail.com>
2022-05-17 12:30:03 +0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import './reset.css';
 import * as Elements from './modules/Dom.js';
 
-Elements.popupBtn.addEventListener('click', (e) => {
+Elements.popupBtn.addEventListener('click', () => {
   const openModal = () => {
     Elements.modal.style.display = 'block';
   };
   openModal();
 });
 
-Elements.closeBtn.addEventListener('click', (e) => {
+Elements.closeBtn.addEventListener('click', () => {
   const closeModal = () => {
     Elements.modal.style.display = 'none';
   };
