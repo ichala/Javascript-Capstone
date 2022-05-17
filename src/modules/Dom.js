508791dfe76d36be57183c58b7849674a2926824
Kwamboka <kwambokaj2.jk@gmail.com>
2022-05-17 20:14:07 +0300

    Add DOM elements

--- src/modules/Dom.js
+++ src/modules/Dom.js
@@ -1,5 +1,5 @@
-const modal = document.querySelector('.popup-window');
-const popupBtn = document.querySelector('.open-popup');
+const modal = document.querySelector('.popup-container');
+const comment = document.querySelector('.comment');
 const closeBtn = document.querySelector('.close-window');
 
-export { modal, popupBtn, closeBtn };
+export { modal, comment, closeBtn };
