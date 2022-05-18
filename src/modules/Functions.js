172e834508fb492def3f4a6c7739556224ba6d39
ichala <alijandhd@gmail.com>
2022-05-18 10:03:14 +0100

    Add Id to buttons

--- src/modules/Functions.js
+++ src/modules/Functions.js
@@ -71,31 +71,31 @@ function DisplayPopup(data, examples) {
 }
 
 function DisplayCards(data) {
-  data.forEach((element) => {
-    Cards.innerHTML += `
-        <div class="card">
-        <div class="img-container">
-            <div class="img-hold">
-                <div class="img">
-                    <img src="${element.strCategoryThumb}">
-                </div>
-            </div>
-        </div>
-        <div class="title-container">
-            <h3>${element.strCategory}</h3>
-            <div class="interactions">
-            <div><i class="fa-solid fa-comment fa-lg"></i>  355  </div>       <div><i class="fa-solid fa-heart fa-lg"></i> 355 </div>
-            </div>
-            <div>
-            ${element.strCategoryDescription.substr(0, 50)}...
-            </div>
-            
-        <button class="button comment"><i class="fa-solid fa-comments"></i> Comments</button>
-        </div>
-
-    </div>
-        `;
-  });
-}
+    data.forEach((element) => {
+      Cards.innerHTML += `
+          <div class="card">
+          <div class="img-container">
+              <div class="img-hold">
+                  <div class="img">
+                      <img src="${element.strCategoryThumb}">
+                  </div>
+              </div>
+          </div>
+          <div class="title-container">
+              <h3>${element.strCategory}</h3>
+              <div class="interactions">
+              <div><i id="${element.idCategory}" class="fa-solid fa-comment fa-lg"></i>  355  </div>       <div><i class="fa-solid fa-heart fa-lg"></i> <b id="${element.idCategory}" class="likes-counter">355</b> </div>
+              </div>
+              <div>
+              ${element.strCategoryDescription.substr(0, 50)}...
+              </div>
+              
+          <button id="${element.idCategory}" class="button comment"><i class="fa-solid fa-comments"></i> Comments</button>
+          </div>
+  
+      </div>
+          `;
+    });
+  }
 
 export { DisplayPopup, DisplayCards };
